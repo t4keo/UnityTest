d5f1ff384ec7a06c4e86fcf83775cdf7864c2838
ESSLINGER Cédric <cedric.esslinger@viacesi.fr>
2014-06-05 17:08:22 +0200

    Save 05.06.2014 17.08
    Gestion du tir(1/2) fini

--- Jeu1/Assets/Scripts/PlayerScript.cs
+++ Jeu1/Assets/Scripts/PlayerScript.cs
@@ -1,15 +1,48 @@
 ﻿using UnityEngine;
-using System.Collections;
 
-public class PlayerScript : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
+/// <summary>
+/// Contrôleur du joueur
+/// </summary>
+public class PlayerScript : MonoBehaviour
+{
+	/// <summary>
+	/// 1 - La vitesse de déplacement
+	/// </summary>
+	public Vector2 speed = new Vector2(50, 50);
 	
+	// 2 - Stockage du mouvement
+	private Vector2 movement;
+	
+	void Update()
+	{
+		// 3 - Récupérer les informations du clavier/manette
+		float inputX = Input.GetAxis("Horizontal");
+		float inputY = Input.GetAxis("Vertical");
+		
+		// 4 - Calcul du mouvement
+		movement = new Vector2(
+			speed.x * inputX,
+			speed.y * inputY);
+
+		// 5 - Tir
+		bool shoot = Input.GetButtonDown("Fire1");
+		shoot |= Input.GetButtonDown("Fire2");
+		// Astuce pour ceux sous Mac car Ctrl + flèches est utilisé par le système
+		
+		if (shoot)
+		{
+			WeaponScript weapon = GetComponent<WeaponScript>();
+			if (weapon != null)
+			{
+				// false : le joueur n'est pas un ennemi
+				weapon.Attack(false);
+			}
+		}
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
+	void FixedUpdate()
+	{
+		// 5 - Déplacement
+		rigidbody2D.velocity = movement;
 	}
 }
